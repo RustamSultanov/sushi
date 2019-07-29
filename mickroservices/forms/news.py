bcc170872e2a7e41690d967121d4dbbf6d7cb544
toomanof <toomanof@gmail.com>
2019-07-29 15:37:33 +0300

    Добавлено поле выбора изображение на странице редактирования новости

--- mickroservices/forms/news.py
+++ mickroservices/forms/news.py
@@ -3,22 +3,18 @@ from django import forms
 from mickroservices.models import NewsPage
 
 
-
 class NewsForm(forms.ModelForm):
     title = forms.CharField()
     body = forms.CharField(widget=forms.HiddenInput())
     content = forms.CharField(widget=forms.HiddenInput())
 
-
     class Meta:
         model = NewsPage
-        fields = ('title', 'first_published_at', 'body', 'content')
-        widgets = {
-                   'first_published_at': forms.TextInput(attrs={
-                   'class': 'form-control datetimepicker-input',
-                   'data-target': '#datetimepicker1',
-                   'placeholder': "Даты занятий",
-                   'wtype': 'date',
-                   'type': 'text'
-                })
-        }        
+        fields = ('title', 'first_published_at', 'image', 'body', 'content')
+        widgets = {'first_published_at': forms.TextInput(attrs={
+            'class': 'form-control datetimepicker-input',
+            'data-target': '#datetimepicker1',
+            'placeholder': "Даты занятий",
+            'wtype': 'date',
+            'type': 'text'})
+        }
